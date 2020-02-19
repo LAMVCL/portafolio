293d2d8841e43d7f992e484b62deeca8999522bc
Luis <luismartinez311@outlook.es>
2020-02-19 05:14:06 -0300

    example image for cards incorporated

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -5,30 +5,30 @@
   <div class="row">
     <div class="col-sm">
       <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
+        <img class="card-img-top" src="{{URL::asset('img/gps.png')}}" alt="Card image cap">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 class="card-title">GPS CHILE</h5>
+            <p class="card-text">Some quick example text to build on the GPS CHILE and make up the bulk of the card's content.</p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
     </div>
     <div class="col-sm">
       <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
+        <img class="card-img-top" src="{{URL::asset('img/gps.png')}}" alt="Card image cap">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 class="card-title">GPS CHILE</h5>
+            <p class="card-text">Some quick example text to build on the GPS CHILE and make up the bulk of the card's content.</p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
     </div>
     <div class="col-sm">
       <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
+        <img class="card-img-top" src="{{URL::asset('img/gps.png')}}" alt="Card image cap">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 class="card-title">GPS CHILE</h5>
+            <p class="card-text">Some quick example text to build on the GPS CHILE and make up the bulk of the card's content.</p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
