5006668a9db8d28e1ccf284801dd15311ad33af9
LAMVCL <luismartinez311@outlook.es>
2020-03-04 02:49:33 -0300

    changed proyect topic

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -2,37 +2,6 @@
 @extends('plantilla')
 @section('seccion')
 <div class="container">
-  <div class="row">
-    <div class="col-sm">
-      <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="{{URL::asset('img/gps.png')}}" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">GPS CHILE</h5>
-            <p class="card-text">Some quick example text to build on the GPS CHILE and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm">
-      <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="{{URL::asset('img/gps.png')}}" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">GPS CHILE</h5>
-            <p class="card-text">Some quick example text to build on the GPS CHILE and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm">
-      <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="{{URL::asset('img/gps.png')}}" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">GPS CHILE</h5>
-            <p class="card-text">Some quick example text to build on the GPS CHILE and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-  </div>
+
 </div>
 @endsection
